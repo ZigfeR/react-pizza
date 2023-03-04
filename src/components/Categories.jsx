5a1bee7082a6fd415fa397d4e7f5dcb0e646a4f1
Jhony <xxxpickxxx@gmail.com>
2023-03-04 15:58:04 +0200

    create structuring to Categoties

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,25 @@
+import React from 'react';
+
 function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  const categories = ['Все', 'Мячные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+
+  const onCllickCategory = (index) => {
+    setActiveIndex(index);
+  };
+
   return (
     <div className="categories">
       <ul>
-        <li>Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((category, index) => (
+          <li
+            key={index}
+            onClick={() => onCllickCategory(index)}
+            className={activeIndex === index ? 'active' : ''}>
+            {category}
+          </li>
+        ))}
       </ul>
     </div>
   );
