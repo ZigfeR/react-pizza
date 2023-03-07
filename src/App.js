d56b6978d22476e2c6ecbfef392c792eb74c8012
Jhony <xxxpickxxx@gmail.com>
2023-03-07 18:26:56 +0200

    add Skeleton logic

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import React from 'react';
 import Categories from './components/Categories';
 import Header from './components/Header';
 import PizzaBlock from './components/PizzaBlock';
+import { Skeleton } from './components/PizzaBlock/Skeleton';
 import Short from './components/Short';
 
 import './scss/app.scss';
 
 function App() {
   const [items, setItems] = React.useState([]);
+  const [isLoading, setLoading] = React.useState(true);
 
   React.useEffect(() => {
     fetch('https://64074338862956433e6a09d1.mockapi.io/items')
       .then((res) => res.json())
       .then((arr) => {
         setItems(arr);
+        setLoading(false);
       });
   }, []);
 
@@ -29,9 +32,9 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {items.map((obj) => (
-              <PizzaBlock key={obj.id} {...obj} />
-            ))}
+            {isLoading
+              ? [...new Array(8)].map((_, index) => <Skeleton key={index} />)
+              : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
           </div>
         </div>
       </div>
