1e34e49aee7c697a93eb45aacbbb9a7757c846a8
Jhony <xxxpickxxx@gmail.com>
2023-03-20 00:15:37 +0200

    fix popout to Sort

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -5,6 +5,7 @@ import { setSort } from '../redux/slices/filterSlice';
 function Sort() {
   const dispatch = useDispatch();
   const sort = useSelector((state) => state.filter.sort);
+  const sortRef = React.useRef();
 
   const [open, setOpen] = React.useState(false);
   const list = [
@@ -21,8 +22,20 @@ function Sort() {
     setOpen(false);
   };
 
+  React.useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (!event.composedPath().includes(sortRef.current)) {
+        setOpen(false);
+      }
+    };
+
+    document.body.addEventListener('click', handleClickOutside);
+
+    return () => document.body.removeListener('click', handleClickOutside);
+  }, []);
+
   return (
-    <div className="sort">
+    <div ref={sortRef} className="sort">
       <div className="sort__label">
         <svg
           width="10"
