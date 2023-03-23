0bb1b29c09990d73a11a19c6521f9d56842f501c
Jhony <xxxpickxxx@gmail.com>
2023-03-23 14:29:50 +0200

    add function selectSort

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { setSort } from '../redux/slices/filterSlice';
+import { selectSort, setSort } from '../redux/slices/filterSlice';
 
 function Sort() {
   const dispatch = useDispatch();
-  const sort = useSelector((state) => state.filter.sort);
+  const sort = useSelector(selectSort);
   const sortRef = React.useRef();
 
   const [open, setOpen] = React.useState(false);
