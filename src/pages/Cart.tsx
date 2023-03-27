7b62afff2208244930e80caca09adb9d11e0f2dd
Jhony <xxxpickxxx@gmail.com>
2023-03-27 22:26:25 +0300

    convert jsx to tsx in a Cart

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import CartItem from './../components/CartItem';
+import CartItem from '../components/CartItem';
 import { clearItems, selectCart } from '../redux/slices/cartSlice';
 import CartEmpty from '../components/CartEmpty';
 
-const Cart = () => {
+const Cart: React.FC = () => {
   const dispatch = useDispatch();
   const { totalPrice, items } = useSelector(selectCart);
-  const totalCount = items.reduce((total, item) => total + item.count, 0);
+  const totalCount = items.reduce((total: number, item: any) => total + item.count, 0);
 
   const onClickClear = () => {
     if (window.confirm('Are you sure you want to remove')) {
@@ -96,7 +96,7 @@ const Cart = () => {
           </div>
         </div>
         <div className="content__items">
-          {items.map((item) => (
+          {items.map((item: any) => (
             <CartItem key={item.id} {...item} />
           ))}
         </div>
