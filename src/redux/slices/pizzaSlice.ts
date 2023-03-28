aa3b54fe5fcde4fbc70c897eae9720c04016c6d1
Jhony <xxxpickxxx@gmail.com>
2023-03-28 15:47:47 +0300

    add enum Status to pizzaSlice

--- src/redux/slices/pizzaSlice.ts
+++ src/redux/slices/pizzaSlice.ts
@@ -12,14 +12,20 @@ type Pizza = {
   rating: number;
 };
 
+export enum Status {
+  LOADING = 'loading',
+  SUCCESS = 'success',
+  ERROR = 'error',
+}
+
 interface PizzaSliceState {
   items: Pizza[];
-  status: 'loading' | 'success' | 'error';
+  status: Status;
 }
 
 const initialState: PizzaSliceState = {
   items: [],
-  status: 'loading', // loading | success | error
+  status: Status.LOADING, // loading | success | error
 };
 
 export const fetchPizzas = createAsyncThunk<Pizza[], Record<string, string>>(
@@ -44,15 +50,15 @@ const pizzaSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder.addCase(fetchPizzas.pending, (state) => {
-      state.status = 'loading';
+      state.status = Status.LOADING;
       state.items = [];
     });
     builder.addCase(fetchPizzas.fulfilled, (state, action) => {
-      state.status = 'success';
+      state.status = Status.SUCCESS;
       state.items = action.payload;
     });
     builder.addCase(fetchPizzas.rejected, (state) => {
-      state.status = 'error';
+      state.status = Status.ERROR;
       state.items = [];
     });
   },
