2f23fac76266eafc6c1da822451510c62b4b17b5
Jhony <xxxpickxxx@gmail.com>
2023-03-30 23:45:42 +0300

    add React.leazy to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,44 @@
+import React, { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import MainLayout from './layouts/MainLayout';
-import Cart from './pages/Cart';
-import FullPizza from './pages/FullPizza';
 import Home from './pages/Home';
-import NotFound from './pages/NotFound';
 
 import './scss/app.scss';
+
+const FullPizza = React.lazy(() => import(/* webpackChunkName: "FullPizza" */ './pages/FullPizza'));
+const Cart = React.lazy(() => import(/* webpackChunkName: "FullPizza" */ './pages/Cart'));
+const NotFound = React.lazy(() => import(/* webpackChunkName: "NotFound" */ './pages/NotFound'));
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
-        <Route path="*" element={<NotFound />} />
+        <Route
+          path="*"
+          element={
+            <Suspense fallback={<div>Идёт загрузка...</div>}>
+              <NotFound />
+            </Suspense>
+          }
+        />
         <Route path="" element={<Home />} />
-        <Route path="pizza/:id" element={<FullPizza />} />
-        <Route path="cart" element={<Cart />} />
+        <Route
+          path="pizza/:id"
+          element={
+            <Suspense fallback={<div>Идёт загрузка...</div>}>
+              <FullPizza />
+            </Suspense>
+          }
+        />
+        <Route
+          path="cart"
+          element={
+            <Suspense fallback={<div>Идёт загрузка корзины...</div>}>
+              <Cart />
+            </Suspense>
+          }
+        />
       </Route>
     </Routes>
   );
