a76833d41a22a0c9060d047058ce775a67752065
Jhony <xxxpickxxx@gmail.com>
2023-03-31 00:19:41 +0300

    add button to fullPizza

--- src/pages/FullPizza.tsx
+++ src/pages/FullPizza.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 const FullPizza: React.FC = () => {
   const [pizza, setPizza] = React.useState<{
@@ -11,23 +11,18 @@ const FullPizza: React.FC = () => {
   const { id } = useParams();
   const navigate = useNavigate();
 
-  React.useEffect(
-    () => {
-      async function fetchPizza() {
-        try {
-          const { data } = await axios.get(
-            'https://64074338862956433e6a09d1.mockapi.io/items/' + id,
-          );
-          setPizza(data);
-        } catch (error) {
-          alert('error.message');
-          navigate('/');
-        }
+  React.useEffect(() => {
+    async function fetchPizza() {
+      try {
+        const { data } = await axios.get('https://64074338862956433e6a09d1.mockapi.io/items/' + id);
+        setPizza(data);
+      } catch (error) {
+        alert('error.message');
+        navigate('/');
       }
-      fetchPizza();
-    }, // eslint-disable-next-line react-hooks/exhaustive-deps
-    [],
-  );
+    }
+    fetchPizza();
+  }, [id, navigate]);
 
   if (!pizza) {
     return <>Downloaded</>;
@@ -37,9 +32,13 @@ const FullPizza: React.FC = () => {
       <img src={pizza.imageUrl} alt="" />
       <h2>{pizza.title}</h2>
       <h4>{pizza.price} грн</h4>
+      <Link to="/">
+        <button className="button button--outline button--add">
+          <span>Назад</span>
+        </button>
+      </Link>
     </div>
   );
-}
-
+};
 
 export default FullPizza;
