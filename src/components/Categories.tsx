c70179bd33a609efababae993675a74fb77dcdc6
Jhony <xxxpickxxx@gmail.com>
2023-04-02 15:00:59 +0300

    fix display Category

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -11,14 +11,23 @@ export const Categories: React.FC<CategoriesProps> = React.memo(
     return (
       <div className="categories">
         <ul>
-          {itemsPizza.map((obj: any) => (
-            <li
-              key={obj.id}
-              onClick={() => onClickCategory(obj.id)}
-              className={value === obj.id ? 'active' : ''}>
-              {obj.name}
-            </li>
-          ))}
+          {value === 0
+            ? itemsPizza.map((obj: any) => (
+                <li
+                  key={obj.id}
+                  onClick={() => onClickCategory(obj.id)}
+                  className={'Все' === obj.name ? 'active' : ''}>
+                  {obj.name}
+                </li>
+              ))
+            : itemsPizza.map((obj: any) => (
+                <li
+                  key={obj.id}
+                  onClick={() => onClickCategory(obj.id)}
+                  className={value === obj.id ? 'active' : ''}>
+                  {obj.name}
+                </li>
+              ))}
         </ul>
       </div>
     );
